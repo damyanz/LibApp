9656b78f502731dc181708208c71b1f999394c0b
Damian Zapalski <damyan.z@o2.pl>
2022-03-04 19:28:34 +0100

    2. Add validation and validation messages to BookForm

--- Models/Book.cs
+++ Models/Book.cs
@@ -9,19 +9,23 @@ namespace LibApp.Models
     public class Book
     {
         public int Id { get; set; }
-		[Required]
+		[Required(ErrorMessage = "Name is required")]
 		[StringLength(255)]
 		public string Name { get; set; }
-		[Required]
+		[Required(ErrorMessage = "Author name is required")]
 		public string AuthorName { get; set; }
-		[Required]
+		[Required(ErrorMessage = "Genre is required")]
 		public Genre Genre { get; set; }
 		public byte GenreId { get; set; }
 		public DateTime DateAdded { get; set; }
 		[Display(Name="Release Date")]
+		[Required(ErrorMessage = "Release date is required")]
 		public DateTime ReleaseDate { get; set; }
 		[Display(Name="Number in Stock")]
+		[Required(ErrorMessage = "Number in stock is required")]
+		[Range(1, 20, ErrorMessage = "Number in stock must be between 1 and 20")]
 		public int NumberInStock { get; set; }
+		[Required(ErrorMessage = "Number available is required.")]
 		public int NumberAvailable { get; set; }
 	}
 
