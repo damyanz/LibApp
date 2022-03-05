774b723101fe0b4bfd9478a83ab7842c76e7ff4d
Damian Zapalski <damyan.z@o2.pl>
2022-03-05 13:35:46 +0100

    3. Implement database communication interface

---
+++ Repositories/BookRepository.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using LibApp.Models;
+using LibApp.Interfaces;
+using LibApp.Data;
+
+namespace LibApp.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public BookRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Book> GetBooks()
+        {
+            return _context.Books;
+        }
+
+        public Book GetBookById(int bookId) {
+            return _context.Books.Find(bookId);
+        }
+    
+        public void AddBook(Book book) {
+            _context.Books.Add(book);
+        }
+
+        public void UpdateBook(Book book)
+        {
+            _context.Books.Update(book);
+        }
+
+        public void DeleteBook(int bookId){
+            _context.Books.Remove(GetBookById(bookId));
+        }
+
+        public void Save() {
+            _context.SaveChanges();
+        }
+    }
+}
